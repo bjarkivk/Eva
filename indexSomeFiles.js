f1a12b7cf56ebe66ab49a19c3f7f60b7e908e5c1
Bjarki Viðar Kristjánsson <bvkr@kth.se>
2022-01-25 11:30:23 +0100

    adding bulk - proof of concept

---
+++ indexSomeFiles.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+const util = require("util");
+
+const jsonstring = fs.readFileSync(
+  __dirname + "/Paragraphs/test.json",
+  "utf-8"
+);
+
+const data = jsonstring;
+console.log(data);
+
+const options = {
+  hostname: "localhost",
+  port: 9200,
+  path: "/paragraphs/_bulk/",
+  method: "PUT",
+  headers: {
+    "Content-Type": "application/json",
+    // 'Content-Length': data.length
+  },
+};
+
+const req = http.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on("data", (d) => {
+    process.stdout.write(d);
+  });
+});
+
+req.on("error", (error) => {
+  console.error(error);
+});
+
+req.write(data);
+req.end();
