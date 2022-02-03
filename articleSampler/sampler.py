51f189fc718576e9ac21d1bdaeb9c0db8eb409fb
Bjarki Viðar Kristjánsson <bvkr@kth.se>
2022-02-03 11:20:25 +0100

    Random sampler works

--- articleSampler/sampler.py
+++ articleSampler/sampler.py
@@ -1,13 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import time
 
 
 # Functions
 
 # Retruns True if we should keep the article, False otherwise
 def should_we_keep_article(soup):
-    h2_elements = soup.find(id="bodyContent").find(id="mw-content-text").find(class_="mw-parser-output").findAll("h2",recursive=False) # find all h2
+    try:
+        h2_elements = soup.find(id="bodyContent").find(id="mw-content-text").find(class_="mw-parser-output").findAll("h2",recursive=False) # find all h2
+    except:
+        print("An exception occurred when finding all h2")
+        return False
     number_of_h2 = len(h2_elements)
     # print(h2_elements)
     # print(number_of_h2)
@@ -25,14 +30,6 @@ def should_we_keep_article(soup):
     if(legal_elements < 2): 
         return False
 
-    cat_list = soup.find("div", id="catlinks").find("ul").find_all("a") # List of categories
-    categories = [x.get_text() for x in cat_list] # get the string
-
-    for i in categories:
-        if i == 'Män' or i == 'Kvinnor': # Discard people
-            return False
-        if i == 'Förgreningssidor': # Discard disambiguation pages
-            return False
     return True
 
 
@@ -43,25 +40,25 @@ def should_we_keep_article(soup):
 # Start of code #
 #################
 
-# Open a txt file for search queries(title/section/...) and for paragraph texts
+# Open a txt file Wikipedia paths
 if(os.path.exists("articles.txt")):
     os.remove("articles.txt")
 articles_file = open("articles.txt", "a")
 
-test=["https://sv.wikipedia.org/wiki/Polisen_i_Finland", "https://sv.wikipedia.org/wiki/Gr%C3%A4nsbevakningsv%C3%A4sendet", "https://sv.wikipedia.org/wiki/F%C3%B6runders%C3%B6kning", "https://sv.wikipedia.org/wiki/The_Beatles"]
+# test=["https://sv.wikipedia.org/wiki/Polisen_i_Finland", "https://sv.wikipedia.org/wiki/Gr%C3%A4nsbevakningsv%C3%A4sendet", "https://sv.wikipedia.org/wiki/F%C3%B6runders%C3%B6kning", "https://sv.wikipedia.org/wiki/The_Beatles", "https://sv.wikipedia.org/wiki/The_Beatles", "https://sv.wikipedia.org/wiki/F%C3%B6runders%C3%B6kning"]
 
-n=len(test)
-# n = 200
+# n=len(test)
+n = 3000 # Number of articles to sample
+start = time.time()
 
-legal_article_count = 0
+set_of_articles = set()
 
 
-# test=["https://sv.wikipedia.org/wiki/Anja_P%C3%A4rson", "https://sv.wikipedia.org/wiki/Mustafa_Sandal", "https://sv.wikipedia.org/wiki/USA"]
+for index in range(n):
+# for index in test:
+    response = requests.get(url="https://slumpartikel.toolforge.org/") # random Swedish Wikipedia article not created by a bot
+    # response = requests.get(url=index)
 
-# for index in range(10):
-for index in test:
-    # response = requests.get(url="https://slumpartikel.toolforge.org/") # random Swedish Wikipedia article not created by a bot
-    response = requests.get(url=index)
     soup = BeautifulSoup(response.content, 'html.parser')
 
     # Get url of random article
@@ -70,16 +67,26 @@ for index in test:
     #Check if we want to discard article
     should_keep = should_we_keep_article(soup) # boolean value returned
     if(should_keep):
-        # Write path of article to file
-        articles_file.write(path)
-        articles_file.write('\n')
-        legal_article_count += 1
+        set_of_articles.add(path)
 
+    print(index+1)
+    print(path)
+
+end = time.time()
 
 print("Sampling finished!")
-print(legal_article_count, "of", n, "articles saved.")
+print(len(set_of_articles), "of", n, "articles saved.")
+end = time.time()
+print("Time elapsed:", end - start)
 print()
 
+
+# Write path of article to file
+print('Writing articles to file...')
+for val in set_of_articles:
+    articles_file.write(val)
+    articles_file.write('\n')
+print('Finished writing.')
 
 # Close file connections
 articles_file.close()
